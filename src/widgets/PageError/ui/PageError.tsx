915c611095d1c773404848e57a04368279683361
placeholder <placeholder>
2024-08-26 21:01:27 +0300

    fix typescripe

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ClassNames } from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './PageError.module.scss';
 
 interface PageErrorProps {
@@ -9,13 +10,14 @@ interface PageErrorProps {
 }
 
 export const PageError:FC<PageErrorProps> = ({ className }) => {
+    const { theme } = useTheme();
     const { t } = useTranslation('translation');
     const reloadPage = () => {
         // eslint-disable-next-line no-restricted-globals
         location.reload();
     };
     return (
-        <div className={ClassNames(cls.PageError, {}, [className])}>
+        <div className={ClassNames(cls.PageError, {}, [className, theme])}>
             {/* eslint-disable-next-line i18next/no-literal-string */}
             <p className={cls.emoticon}>(,,&gt;﹏&lt;,,)</p>
             <p>
