075ede08528772beaefe8321495d2eca4b217ab6
placeholder <placeholder>
2024-08-30 23:44:57 +0300

    new+++

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,17 @@ import './styles/index.scss';
 
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
+import { NotFoundPage } from 'pages/NotFoundPage';
 import { Suspense } from 'react';
 import { ClassNames } from 'shared/lib/classNames/classNames';
-import { Navbar } from 'widgets/Navbar/ui/Navbar';
+import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
     const { theme } = useTheme();
     return (
         <div className={ClassNames('app', {}, [theme])}>
+            <NotFoundPage />
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
