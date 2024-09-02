cde106ecf9a53f589a99426865fd23fc55b1b0d9
placeholder <placeholder>
2024-09-02 14:47:01 +0300

    approved

--- src/shared/config/tests/componentRender/componentRender.tsx
+++ src/shared/config/tests/componentRender/componentRender.tsx
@@ -4,9 +4,14 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import i18n from 'shared/config/i18n/i18nForTests';
 
-export function componentRender(component: ReactNode) {
+interface componentRenderOptions {
+    route: string
+}
+
+export function componentRender(component: ReactNode, options:
+     componentRenderOptions = { route: '/' }) {
     return render(
-        <MemoryRouter>
+        <MemoryRouter initialEntries={[options.route]}>
             <I18nextProvider i18n={i18n}>
                 {component}
             </I18nextProvider>
--- src/shared/config/tests/renderWithRouter/renderWithRouter.tsx
+++ src/shared/config/tests/renderWithRouter/renderWithRouter.tsx
@@ -2,9 +2,14 @@ import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
-export function renderWithRouter(component: ReactNode) {
+interface renderWithRouterOptions {
+    route: string
+}
+
+export function renderWithRouter(component: ReactNode, options:
+    renderWithRouterOptions = { route: '/' }) {
     return render(
-        <MemoryRouter>
+        <MemoryRouter initialEntries={[options.route]}>
             {component}
         </MemoryRouter>
     );
